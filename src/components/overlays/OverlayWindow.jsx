d689503adf0c437b365b9dc43b1fb0e8ef8da839
Ariel Abad <arielabad@Ariels-MacBook-Pro.local>
2023-12-27 13:39:27 -0600

    fix ios scroll

--- src/components/overlays/OverlayWindow.jsx
+++ src/components/overlays/OverlayWindow.jsx
@@ -130,7 +130,7 @@ function OverlayWindow({user, show, setUserData}) {
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 w-screen min-h-full overflow-y-auto">
+        <div className="fixed min-h-full inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
@@ -141,7 +141,7 @@ function OverlayWindow({user, show, setUserData}) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="min-h-full relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:min-h-full">
                 <div className="bg-white dark:bg-gray-700 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -151,7 +151,7 @@ function OverlayWindow({user, show, setUserData}) {
                         alt=""
                       />
                     </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <div className="min-h-full mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title as="h3" className="text-base font-semibold text-lg leading-6 text-gray-900 dark:text-gray-300">
                         FIRE Journey Dashboard
                       </Dialog.Title>
